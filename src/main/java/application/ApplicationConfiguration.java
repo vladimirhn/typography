906a510e20195501ed35b1074ad06b98952f08ef
Karsanov <vk@kalite.ro>
2021-06-27 13:58:48 +0300

    Автозапуск браузера

---
+++ src/main/java/application/ApplicationConfiguration.java
@@ -0,0 +1,40 @@
+package application;
+
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.event.EventListener;
+
+import java.awt.*;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+@Configuration
+public class ApplicationConfiguration {
+
+    @EventListener
+    public void onApplicationReadyEvent(ApplicationReadyEvent event) throws URISyntaxException, IOException {
+//        if (Desktop.isDesktopSupported() && Desktop.getDesktop().isSupported(Desktop.Action.BROWSE)) {
+//            Desktop.getDesktop().browse(new URI("http://localhost"));
+//        }
+        String url = "http://localhost:8080";
+
+        if(Desktop.isDesktopSupported()){
+            Desktop desktop = Desktop.getDesktop();
+            try {
+                desktop.browse(new URI(url));
+            } catch (IOException | URISyntaxException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }else{
+            Runtime runtime = Runtime.getRuntime();
+            try {
+                runtime.exec("xdg-open " + url);
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
+    }
+}

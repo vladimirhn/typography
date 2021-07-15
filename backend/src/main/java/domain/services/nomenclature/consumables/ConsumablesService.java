9cfadc391eda7814c686e91573b7164a55f1cf56
Karsanov <vk@kalite.ro>
2021-07-15 17:02:01 +0300

    Рефакторинг ConsumablesService

--- backend/src/main/java/domain/services/nomenclature/consumables/ConsumablesService.java
+++ backend/src/main/java/domain/services/nomenclature/consumables/ConsumablesService.java
@@ -28,42 +28,50 @@ public class ConsumablesService extends TypoViewService<ConsumablesViewLine> {
 
     public Map<Long, JsonConsumableType> createConsumableTypesResponse() {
 
+        Map<Long, KList<ConsumablesViewLine>> groupByTypeId = findAll().groupBy(ConsumablesViewLine::getTypeId);
+
+        return createResult(groupByTypeId);
+    }
+
+    private Map<Long, JsonConsumableType> createResult(Map<Long, KList<ConsumablesViewLine>> groupByTypeId) {
+
         Map<Long, JsonConsumableType> result = new TreeMap<>();
 
-        Map<Long, KList<ConsumablesViewLine>> groupByTypeId = findAll().groupBy(ConsumablesViewLine::getTypeId);
-        groupByTypeId.forEach((typeId, typeLines) -> {
-
-            JsonConsumableType typeEntry = new JsonConsumableType();
-            typeEntry.setType(typeLines.getAny().getTypeName());
-            typeEntry.setProperties(new TreeMap<>());
-            typeEntry.setData(new TreeMap<>());
-
-            Map<Long, KList<ConsumablesViewLine>> groupByPropertyId = typeLines.groupByWithNulls(ConsumablesViewLine::getPropertyId);
-            groupByPropertyId.forEach((propId, propLines) -> typeEntry.getProperties().put(propId, propLines.getAny().getPropertyName()));
-
-            Map<Long, KList<JsonConsumableItem>> data = new TreeMap<>();
-            Map<Long, KList<ConsumablesViewLine>> groupByItemId = typeLines.groupByWithNulls(ConsumablesViewLine::getItemId);
-            groupByItemId.forEach((itemId, itemLines) -> {
-                JsonConsumableItem itemEntry = new JsonConsumableItem();
-                itemEntry.setItem(itemLines.getAny().getItemName());
-                itemEntry.setValues(new TreeMap<>());
-
-                itemLines.forEach(itemLine -> {
-                    if (itemLine.getPropertyId() != null && itemLine.getValueId() != null) {
-                        itemEntry.getValues().put(itemLine.getPropertyId(), Collections.singletonMap(itemLine.getValueId(), itemLine.getValueValue()));
-                    }
-                });
-
-                if (!data.containsKey(itemId)) data.put(itemId, CollectionFactory.makeLinkedList());
-
-                data.get(itemId).add(itemEntry);
-            });
-
-            typeEntry.setData(data);
-            result.put(typeId, typeEntry);
-        });
-
+        groupByTypeId.forEach((Long typeId, KList<ConsumablesViewLine> typeLines) ->
+                processEachType(result, typeId, typeLines));
 
         return result;
+    }
+
+    private void processEachType(Map<Long, JsonConsumableType> result, Long typeId, KList<ConsumablesViewLine> typeLines) {
+
+        JsonConsumableType typeEntry = new JsonConsumableType();
+        typeEntry.setType(typeLines.getAny().getTypeName());
+        typeEntry.setProperties(new TreeMap<>());
+        typeEntry.setData(new TreeMap<>());
+
+        Map<Long, KList<ConsumablesViewLine>> groupByPropertyId = typeLines.groupByWithNulls(ConsumablesViewLine::getPropertyId);
+        groupByPropertyId.forEach((propId, propLines) -> typeEntry.getProperties().put(propId, propLines.getAny().getPropertyName()));
+
+        Map<Long, KList<JsonConsumableItem>> data = new TreeMap<>();
+        Map<Long, KList<ConsumablesViewLine>> groupByItemId = typeLines.groupByWithNulls(ConsumablesViewLine::getItemId);
+        groupByItemId.forEach((itemId, itemLines) -> {
+            JsonConsumableItem itemEntry = new JsonConsumableItem();
+            itemEntry.setItem(itemLines.getAny().getItemName());
+            itemEntry.setValues(new TreeMap<>());
+
+            itemLines.forEach(itemLine -> {
+                if (itemLine.getPropertyId() != null && itemLine.getValueId() != null) {
+                    itemEntry.getValues().put(itemLine.getPropertyId(), Collections.singletonMap(itemLine.getValueId(), itemLine.getValueValue()));
+                }
+            });
+
+            if (!data.containsKey(itemId)) data.put(itemId, CollectionFactory.makeLinkedList());
+
+            data.get(itemId).add(itemEntry);
+        });
+
+        typeEntry.setData(data);
+        result.put(typeId, typeEntry);
     }
 }

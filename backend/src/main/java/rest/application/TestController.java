51f9e5b433bc7d8194ce5c48daf31031d5d51da9
Karsanov <vk@kalite.ro>
2021-07-27 11:26:18 +0300

    Логгирование

--- backend/src/main/java/rest/application/TestController.java
+++ backend/src/main/java/rest/application/TestController.java
@@ -4,6 +4,8 @@ import domain.models.nomenclature.consumables.ConsumableItem;
 import domain.services.abstracts.response.KResponseComposer;
 import domain.services.abstracts.response.SimpleTableResponse;
 import domain.services.nomenclature.consumables.ConsumableItemsService;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -19,8 +21,17 @@ public class TestController {
     @Autowired
     KResponseComposer responseComposer;
 
+    Logger logger = LogManager.getLogger(TestController.class);
+
     @GetMapping("/test")
     public SimpleTableResponse get() {
+
+
+
+        logger.debug("hello there!");
+        System.out.println("Hello from System.out");
+
+        if (true) throw new NullPointerException();
 
         List<ConsumableItem> items = consumableItemsService.selectAll();
         items.sort(Comparator.comparing(ConsumableItem::getItem));
--- src/main/java/application/Typography.java
+++ src/main/java/application/Typography.java
@@ -1,5 +1,9 @@
 package application;
 
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.io.IoBuilder;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.Bean;
@@ -13,7 +17,19 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @ComponentScan({"application","domain","rest"})
 public class Typography {
 
+    static {
+//        System.setProperty("log4j.configurationFile", "logger.xml");
+    }
+
     public static void main(String[] args) {
+
+        System.out.println("Working Directory = " + System.getProperty("user.dir"));
+
+        Logger logger = LogManager.getLogger(Typography.class);
+        logger.debug("Try again");
+
+        System.setErr(IoBuilder.forLogger(LogManager.getLogger("consoleErr")).setLevel(Level.ERROR).buildPrintStream());
+        System.setOut(IoBuilder.forLogger(LogManager.getLogger("consoleOut")).setLevel(Level.DEBUG).buildPrintStream());
 
         SpringApplication.run(Typography.class, args);
     }
@@ -23,7 +39,7 @@ public class Typography {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**").allowedOrigins("http://localhost:3000");
+                registry.addMapping("/**").allowedOrigins("*");
             }
         };
     }
